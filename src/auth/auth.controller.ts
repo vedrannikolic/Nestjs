49460afa8afd342278fe87714eb3c43dbdcccf4a
vedrannikolic <n.vedran1989@icloud.com>
2023-10-09 15:02:34 +0200

    First commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,11 +7,11 @@ export class AuthController {
 
   @Post('signup')
   signup() {
-    return 'I am signup';
+    return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return 'I am signin in';
+    return this.authService.signin();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  login() {}
+  signup() {
+    return { msg: 'I am signup' };
+  }
 
-  signup() {}
+  signin() {
+    return { msg: 'I am signin in' };
+  }
 }
