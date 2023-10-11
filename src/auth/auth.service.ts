e231cd0090f2866d68e2c7118705adc5277170cb
vedrannikolic <n.vedran1989@icloud.com>
2023-10-11 15:26:41 +0200

    Signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // generate the hash of the password
+    const hash = await argon.hash(dto.password);
 
     // save the user in the database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return the user
-    return { msg: 'I am signup' };
+      delete user.hash;
+
+      // return the user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
